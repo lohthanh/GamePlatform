99741c7e5b3d2e561ccf8f0d442cc3fbe2d1cd3f
Thanh Lo <lohthanh@gmail.com>
2023-07-27 14:38:33 -0700

    added view one page

--- Data/GameModels.cs
+++ Data/GameModels.cs
@@ -6,11 +6,31 @@ public record class Games(
 );
 
 public record class GameResults(
+    [property: JsonPropertyName("id")] int Id,
     [property: JsonPropertyName("name")] string Name,
     [property: JsonPropertyName("background_image")] string Image,
     [property: JsonPropertyName("genres")] GetGenre[] Genre
 );
 
 public record class GetGenre(
+    [property: JsonPropertyName("name")] string Name
+);
+
+public record class Game(
+    [property: JsonPropertyName("id")] int Id,
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("description")] string Description,
+    [property: JsonPropertyName("released")] string ReleasedDate,
+    [property: JsonPropertyName("background_image")] string Image,
+    [property: JsonPropertyName("platforms")] GetPlatformsSet[] Platforms,
+    [property: JsonPropertyName("genres")] GetGenre[] Genre,
+    [property: JsonPropertyName("website")] string Website
+);
+
+public record class GetPlatformsSet(
+    [property: JsonPropertyName("platform")] GetPlatform Platform
+);
+
+public record class GetPlatform(
     [property: JsonPropertyName("name")] string Name
 );
